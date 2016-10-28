b104a88fd4a3461bb9805193b4e0730119397091
csyntax <csyntax@outlook.com>
2016-10-28 15:19:27 +0300

    Delete Query for models

---
+++ BlogSystem.Data.Common/IDeletableEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BlogSystem.Data.Common
+{
+    public interface IDeletableEntity
+    {
+        bool IsDeleted { get; set; }
+        DateTime? DeletedOn { get; set; }
+    }
+}
