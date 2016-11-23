a8bc9723fd626928cc3412eb172c66f875d159cb
csyntax <csyntax@outlook.com>
2016-11-23 21:47:47 +0200

    Pages controller

--- BlogSystem.Web/Areas/Administration/Controllers/PagesController.cs
+++ BlogSystem.Web/Areas/Administration/Controllers/PagesController.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BlogSystem.Data.UnitOfWork;
 
 namespace BlogSystem.Web.Areas.Administration.Controllers
 {
-    public class PagesController : Controller
+    public class PagesController : AdministrationController
     {
+        public PagesController(IBlogSystemData data) 
+            : base(data)
+        {
+        }
+
         // GET: Administration/Pages
         public ActionResult Index()
         {
-            return View();
+            var pages = this.Data.Pages.All().ToList();
+
+            return View(pages);
         }
     }
 }
