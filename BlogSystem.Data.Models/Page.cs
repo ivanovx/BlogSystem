98f599e88f9c8a0c898f109a296e02ea8a0baf1b
csyntax <csyntax@outlook.com>
2016-11-23 21:40:01 +0200

    IAuditInfo

--- BlogSystem.Data.Models/Page.cs
+++ BlogSystem.Data.Models/Page.cs
@@ -1,9 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using BlogSystem.Data.Contracts;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BlogSystem.Data.Models
 {
-    public class Page
+    public class Page : AuditInfo
     {
         [Key]
         public int Id { get; set; }
