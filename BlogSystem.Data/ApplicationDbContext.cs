5b83268cb585e58345b70c57470f319b637bbb0c
csyntax <csyntax@outlook.com>
2016-11-24 20:53:57 +0200

    Entity

--- BlogSystem.Data/ApplicationDbContext.cs
+++ BlogSystem.Data/ApplicationDbContext.cs
@@ -36,7 +36,10 @@ namespace BlogSystem.Data
 
         private void ApplyAuditInfoRules()
         {
-            IEnumerable<DbEntityEntry> entrySet = this.ChangeTracker.Entries().Where(e => e.Entity is IAuditInfo && ((e.State == EntityState.Added) || (e.State == EntityState.Modified)));
+            var entrySet = this.ChangeTracker
+                .Entries()
+                .Where(e => 
+                    e.Entity is IAuditInfo && ((e.State == EntityState.Added) || (e.State == EntityState.Modified)));
 
             foreach (var entry in entrySet)
             {
