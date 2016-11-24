cdef3e2ce0b36ec5040a2f62d6cd42564c2ad98a
csyntax <csyntax@outlook.com>
2016-11-24 22:17:16 +0200

    Pages routes

---
+++ BlogSystem.Web/Controllers/PagesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BlogSystem.Data.UnitOfWork;
+using BlogSystem.Web.ViewModels.Page;
+using BlogSystem.Web.Infrastructure.Mapping;
+
+namespace BlogSystem.Web.Controllers
+{
+    public class PagesController : BaseController
+    {
+        public PagesController(IBlogSystemData data) 
+            : base(data)
+        {
+        }
+
+        // GET: Pages
+        [HttpGet]
+        public ActionResult Page(int id)
+        {
+            var page = this.Data.Pages.All()
+                .Where(x => x.Id == id)
+                .To<PageViewModel>()
+                .FirstOrDefault();
+
+            return View(page);
+        }
+    }
+}
---
+++ BlogSystem.Web/ViewModels/Page/PageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using BlogSystem.Web.Areas.Administration.ViewModels.BlogPost;
+using BlogSystem.Web.Infrastructure.Mapping;
+
+namespace BlogSystem.Web.ViewModels.Page
+{
+    public class PageViewModel : IMapFrom<Data.Models.Page>, IHaveCustomMappings
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime LastModified { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<Data.Models.Page, PageViewModel>()
+                .ForMember(model => model.LastModified, config => config.MapFrom(page => page.ModifiedOn ?? page.CreatedOn));
+        }
+    }
+}
