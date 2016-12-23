8d3965b8e1c4d9b69da6f7d74e3bbc9d0c8b8f82
csyntax <csyntax@outlook.com>
2016-12-23 14:28:49 +0200

    Settigs - BUG

--- BlogSystem.Web/Areas/Administration/Controllers/SettingsController.cs
+++ BlogSystem.Web/Areas/Administration/Controllers/SettingsController.cs
@@ -1,9 +1,8 @@
-﻿using BlogSystem.Data.Models;
-
-namespace BlogSystem.Web.Areas.Administration.Controllers
+﻿namespace BlogSystem.Web.Areas.Administration.Controllers
 {
     using System.Linq;
     using System.Web.Mvc;
+    using Data.Models;
     using Data.UnitOfWork;
 
     public class SettingsController : AdministrationController
@@ -22,14 +21,38 @@ namespace BlogSystem.Web.Areas.Administration.Controllers
             return this.View(settings);
         }
 
+        [HttpGet]
+        public ActionResult Edit(string key)
+        {
+            /*if (key == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }*/
+
+            var setting = this.data.Settings.All().FirstOrDefault(s => s.Key == key);
+
+            /*if (setting == null)
+            {
+                return this.HttpNotFound();
+            }*/
+
+
+            return this.View(setting);
+        }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Update(Setting setting)
+        public ActionResult Edit(Setting setting)
         {
-            this.data.Settings.Update(setting);
-            this.data.SaveChanges();
+            if (ModelState.IsValid)
+            {
+                this.data.Settings.Update(setting);
+                this.data.Settings.SaveChanges();
 
-            return this.RedirectToAction("Index");
+                return this.RedirectToAction("Index");
+            }
+
+            return this.View(setting);
         }
     }
 }
