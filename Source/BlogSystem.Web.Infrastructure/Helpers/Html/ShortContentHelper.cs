d8f2a92b661c47451bc57c9e9f04dd6a4a12dddd
Ivan Ivanov <csyntax@outlook.com>
2017-01-12 18:13:38 +0200

    short content helper using AngleSharp

---
+++ Source/BlogSystem.Web.Infrastructure/Helpers/Html/ShortContentHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace BlogSystem.Web.Infrastructure.Helpers.Html
+{
+    using System.Web.Mvc;
+    using AngleSharp.Parser.Html;
+
+    public static class ShortContentHelper
+    {
+        public static string Truncate(this HtmlHelper helper, string text)
+        {
+            var parser = new HtmlParser();
+            var document = parser.Parse(text);
+
+            return document.QuerySelector("p").InnerHtml;
+        }
+    }
+}
