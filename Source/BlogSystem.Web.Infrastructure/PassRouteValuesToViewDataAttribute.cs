74e9bb6d02cd59d8b8320757401a8797a68673ba
Ivan Ivanov <csyntax@outlook.com>
2017-01-13 20:44:22 +0200

    Filter for ViewData

--- Source/BlogSystem.Web.Infrastructure/PassRouteValuesToViewDataAttribute.cs
+++ Source/BlogSystem.Web.Infrastructure/PassRouteValuesToViewDataAttribute.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BlogSystem.Web.Infrastructure
+﻿namespace BlogSystem.Web.Infrastructure
 {
     using System.Web.Mvc;
 
@@ -14,6 +8,7 @@ namespace BlogSystem.Web.Infrastructure
         {
             var routes = filterContext.RouteData.Values;
             var viewData = filterContext.Controller.ViewData;
+
             foreach (var route in routes)
             {
                 viewData.Add(route.Key, route.Value);
