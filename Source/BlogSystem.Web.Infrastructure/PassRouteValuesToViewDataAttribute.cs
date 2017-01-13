a14635d5a27a6ed1d0b6e46d48d75c2e55c15239
Ivan Ivanov <csyntax@outlook.com>
2017-01-13 16:36:04 +0200

    pass route vaalues in views

---
+++ Source/BlogSystem.Web.Infrastructure/PassRouteValuesToViewDataAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogSystem.Web.Infrastructure
+{
+    using System.Web.Mvc;
+
+    public class PassRouteValuesToViewDataAttribute : ActionFilterAttribute, IActionFilter
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var routes = filterContext.RouteData.Values;
+            var viewData = filterContext.Controller.ViewData;
+            foreach (var route in routes)
+            {
+                viewData.Add(route.Key, route.Value);
+            }
+        }
+    }
+}
