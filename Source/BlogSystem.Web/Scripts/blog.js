25268a061a4a1b3cd7fa2c674bf97a73cceb28fe
Ivan Ivanov <csyntax@outlook.com>
2017-02-12 10:10:25 +0200

    update blog styles and scripting

--- Source/BlogSystem.Web/Scripts/blog.js
+++ Source/BlogSystem.Web/Scripts/blog.js
@@ -29,23 +29,14 @@ $(window).scroll(function () {
     }
 });
 
+/*
 $(document).ready(function() {
     $("html").niceScroll({
-        cursorcolor: "rgb(49, 50, 51)", // change cursor color in hex
-        cursoropacitymin: 0, // change opacity when cursor is inactive (scrollabar "hidden" state), range from 1 to 0
-        cursoropacitymax: 1, // change opacity when cursor is active (scrollabar "visible" state), range from 1 to 0
-        cursorwidth: "10px", // cursor width in pixel (you can also write "5px")
-        cursorborder: "1px solid #fff", // css definition for cursor border
-        cursorborderradius: "5px", // border radius in pixel for cursor
-        zindex: 11,  // change z-index for scrollbar div
-        scrollspeed: 60, // scrolling speed
-        mousescrollstep: 40, // scrolling speed with mouse wheel (pixel)
-        touchbehavior: false, // enable cursor-drag scrolling like touch devices in desktop computer
-        hwacceleration: true, // use hardware accelerated scroll when supported
-        boxzoom: false, // enable zoom for box content
-        dblclickzoom: true, // (only when boxzoom=true) zoom activated when double click on box
-        gesturezoom: true, // (only when boxzoom=true and with touch devices) zoom activated when pinch out/in on box
-        grabcursorenabled: true, // (only when touchbehavior=true) display "grab" icon
-        autohidemode: false
+        cursorcolor: "rgb(49, 50, 51)",
+        cursorwidth: "10px",
+        cursorborderradius: "5px",
+        scrollspeed: 60,
+        mousescrollstep: 40
+    });
 });
-});
+*/
