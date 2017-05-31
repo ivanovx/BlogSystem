747c113700fc1329aed166603a180fe387f3f300
Ivan Ivanov <csyntax@outlook.com>
2017-05-31 18:05:28 +0300

    Create Attribute for check if ajax request

---
+++ src/BlogSystem.Web.Infrastructure/Attributes/AjaxOnlyAttribute.cs
@@ -0,0 +1,16 @@
+﻿namespace BlogSystem.Web.Infrastructure.Attributes
+{
+    using System;
+    using System.Reflection;
+    using System.Web.Mvc;
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class AjaxOnlyAttribute : ActionMethodSelectorAttribute
+    {
+        public override bool IsValidForRequest(ControllerContext controllerContext, 
+            MethodInfo methodInfo)
+        {
+            return controllerContext.RequestContext.HttpContext.Request.IsAjaxRequest();
+        }
+    }
+}
