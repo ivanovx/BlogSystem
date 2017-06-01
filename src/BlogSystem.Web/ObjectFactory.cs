4c2640bd23e8dd8ec6e6b42df8c1e4fb10801550
Ivan Ivanov <csyntax@outlook.com>
2017-06-01 22:29:08 +0300

    create ObejctFactory for AutoFac DI

---
+++ src/BlogSystem.Web/ObjectFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace BlogSystem.Web
+{
+    using Autofac;
+
+    public class ObjectFactory
+    {
+        private static IContainer container = AutofacConfig.Container;
+
+        public static T GetInstance<T>() where T : class
+        {
+            using (var con = container.BeginLifetimeScope())
+            {
+                return con.Resolve<T>();
+            }
+        }
+    }
+}
